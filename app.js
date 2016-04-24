ba617ed26b816ba3bec4509d3929ed3389b92970
Andrew Furth <andrew.furth@gmail.com>
2016-04-24 15:40:36 -0700

    basic google oAuth configured

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const morgan = require("morgan");
 const routes = require('./routes');
 const session = require('cookie-session');
 const flash = require('connect-flash');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 
 app.set('view engine', 'jade');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
+app.use(morgan("dev"))
 app.use(express.static(__dirname + '/public'));
 
 //get auth.js module
@@ -31,15 +33,25 @@ app.use(flash());
 //***************************************************************************
 //GOOGLE oAuth -- Should think about moving to a separate JS file later to abstract code
 //***************************************************************************
+passport.serializeUser(function(user, done) {
+  //later this will be where you selectively send to the browser an identifier for your user, like their primary key from the database, or their ID from linkedin
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  //here is where you will go to the database and get the user each time from it's id, after you set up your db
+  done(null, obj);
+});
+
+
 passport.use(new GoogleStrategy({
-    consumerKey: process.env.GOOGLE_API_KEY,
-    consumerSecret: process.env.GOOGLE_SECRET_KEY,
+    clientID: process.env.GOOGLE_API_KEY,
+    clientSecret: process.env.GOOGLE_SECRET_KEY,
     callbackURL: "http://localhost:3000/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
-       User.findOrCreate({ googleId: profile.id }, function (err, user) {
-         return done(err, user);
-       });
+    console.log(profile);
+    return done(null, profile);
   }
 ));
 
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 router.route('/login')
 	.get((req, res)=>{
@@ -15,5 +15,27 @@ router.route('/logout')
 	.get((req, res)=>{
 		
 	});
+
+router.route('/google')
+  .get(passport.authenticate('google', {scope: 'openid profile email'}))
+
+router.get('/google/callback', passport.authenticate('google', {
+  successRedirect: '/',
+  failureRedirect: '/'
+}));
+
+// router.route('/google/callback')
+//   .get((req,res,next) => {
+
+//   }
+
+//     passport.authenticate('google', 
+//     // If failure, return to login
+//     { failureRedirect: '/login' }),
+//   function(req, res, next) {
+//     res.redirect('/');
+//   }
+
+//   )
 
 module.exports = router;
