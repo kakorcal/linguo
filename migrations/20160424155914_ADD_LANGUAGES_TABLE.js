a446f6c72695de09252d033d1855603657488ac8
Thomas O'Brien <thomasobrien99@gmail.com>
2016-04-24 16:15:03 -0700

    consolidate language tables into one

--- migrations/20160424155914_ADD_LANGUAGES_TABLE.js
+++ migrations/20160424155914_ADD_LANGUAGES_TABLE.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('languages', table=>{
+  	table.increments();
+  	table.integer('user_id');
+  	table.string('languages');
+  	table.integer('proficiency');
+  	table.string('approach');
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schama.dropTable('languages');
 };
