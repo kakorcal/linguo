dbe2c7f22da3e72fb39bde47e942490263fcca7e
Andrew Furth <andrew.furth@gmail.com>
2016-04-25 16:49:34 -0700

    error handling implemented in app.js

--- app.js
+++ app.js
@@ -32,9 +32,15 @@ app.use(flash());
 require('./helpers/passport')(passport);
 app.use(authHelpers.currentUser);
 
+app.use('/users', routes.users);
+app.use('/auth', routes.auth);
+app.use('/threads', routes.threads);
+app.use('/languages', routes.languages);
+
+
 app.get('/', (req,res)=>{
   // Define flash msgs
-	var logoutMsg = req.flash('logoutMessage');
+  var logoutMsg = req.flash('logoutMessage');
   var notLoggedInMsg = req.flash('notLoggedIn');
   // Determine which (if any) flash message has been passed through
   if (!!logoutMsg.length) {
@@ -51,10 +57,40 @@ app.get('/', (req,res)=>{
   }
 });
 
-app.use('/users', routes.users);
-app.use('/auth', routes.auth);
-app.use('/threads', routes.threads);
-app.use('/languages', routes.languages);
+
+//***************************************************************************
+//From Elie's Knex Photos App https://github.com/gSchool/knex_photos_app/blob/part2solution/app.js
+//***************************************************************************
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 app.listen(3000, ()=>{
 	console.log('Server listening on port 3000...');
