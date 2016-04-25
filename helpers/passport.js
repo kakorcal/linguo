a019111988e79cc8d44227a77ea1a6f3e14f2990
Andrew Furth <andrew.furth@gmail.com>
2016-04-24 17:52:07 -0700

    users login logic implemented; if no location, redirect to edit; if location, redirect to user show page

--- helpers/passport.js
+++ helpers/passport.js
@@ -38,11 +38,13 @@ module.exports = (passport) => {
 
   passport.serializeUser(function(user, done) {
     //later this will be where you selectively send to the browser an identifier for your user, like their primary key from the database, or their ID from linkedin
+    console.log("SERIALIZE")
     done(null, user);
   });
 
   passport.deserializeUser(function(obj, done) {
     //here is where you will go to the database and get the user each time from it's id, after you set up your db
+    console.log("DESERIALIZE")
     done(null, obj);
   });
 }
