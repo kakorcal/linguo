f342ee240473f33de6f369a84dda38653a9c5005
Kenneth Korcal <kakorcal@gmail.com>
2016-04-24 18:10:32 -0700

    clean up fonts directory

--- app.js
+++ app.js
@@ -30,6 +30,32 @@ app.use(passport.session());
 app.use(flash());
 
 require('./helpers/passport')(passport);
+//***************************************************************************
+//GOOGLE oAuth -- Should think about moving to a separate JS file later to abstract code
+//***************************************************************************
+passport.serializeUser(function(user, done) {
+  //later this will be where you selectively send to the browser an identifier for your user, like their primary key from the database, or their ID from linkedin
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  //here is where you will go to the database and get the user each time from it's id, after you set up your db
+  done(null, obj);
+});
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_API_KEY,
+    clientSecret: process.env.GOOGLE_SECRET_KEY,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // const user = profile._json;
+    // console.log(profile)
+    // console.log("EMAILSSSSSSS",profile.emails);
+    return done(null, profile);
+  }
+));
 
 
 app.get('/', (req,res)=>{
