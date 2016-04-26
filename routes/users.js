58339ba66555f22a2506cacec9889e455b470923
Andrew Furth <andrew.furth@gmail.com>
2016-04-25 19:33:17 -0700

    users show messages displays; knex query written and stored in comments, saved for tmrw

--- routes/users.js
+++ routes/users.js
@@ -37,17 +37,27 @@ router.route('/:id/edit')
 
 router.route('/:id')
 	.get(authHelpers.ensureCorrectUser, (req, res)=>{
-		knex('users')
-		.where('id', req.params.id)
-		.first()
-		.then(user=>{
-			knex('languages')
-			.where('user_id', user.id)
-			.then(languages => {
-				res.render('users/show', {user, languages, message: req.flash('loginMessage')});
-			});
-		});
+		knex('messages as m')
+			.select('u.id as uid', 'm.id as mid', 'm.thread_id', 'm.sender_id', 'u.email as send_email', 'm.rec_id', 't.subject')
+			.join('threads as t', 'm.thread_id', 't.id')
+			.join('users as u', 'm.sender_id', 'u.id')
+			.where('m.sender_id', req.params.id)
+			.orWhere('m.rec_id', req.params.id)
+			.then((usersMsgs) => {
+				res.render('users/show', {usersMsgs, message: req.flash('loginMessage')});
+			})
 	})
+
+	// language_app=# select us.id as uid,  m.id as mid, m.thread_id, m.sender_id, us.email as send_email, m.rec_id, ur.email as rec_email,  t.subject     
+	// from messages as m
+	// join threads as t
+	// on m.thread_id=t.id
+	// join users as us
+	// on m.sender_id=us.id
+	// join users as ur
+	// on m.rec_id=ur.id
+	// where m.sender_id=5 or m.rec_id=5;
+
 	.put(authHelpers.ensureCorrectUser, (req, res)=>{
 			knex('users')
 			.where('id', req.params.id)
