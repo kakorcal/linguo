7f7e83887b63f2d95a4ea801057eb8baf1755f82
Andrew Furth <andrew.furth@gmail.com>
2016-04-25 17:32:46 -0700

    latest migrations run

--- routes/threads.js
+++ routes/threads.js
@@ -21,6 +21,7 @@ router.route('/')
 			//INSERTS A MESSAGE INTO THE MESSAGES TABLE BASED ON THE THREAD ID
 			//CHECK WHAT MESSAGE LOOKS LIKE
 			var message = req.body.message;
+
 			knex('messages')
 			.insert(
 			{
@@ -35,6 +36,7 @@ router.route('/')
 				knex('thread_participants')
 				.insert({thread_id:thread_id[0], user_id:data[0].sender_id})
 				.then(()=>{
+
 			  	knex('thread_participants')
 			  	.insert({thread_id:thread_id[0], user_id:data[0].rec_id})
 			  	.then(()=>{})
