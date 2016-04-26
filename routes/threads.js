1519652b3a76bfe51aa3f1b7c3648d63ec6b186e
Thomas O'Brien <thomasobrien99@gmail.com>
2016-04-25 17:40:22 -0700

    de-locus-ify

--- routes/threads.js
+++ routes/threads.js
@@ -15,7 +15,6 @@ router.route('/')
 			//INSERTS A MESSAGE INTO THE MESSAGES TABLE BASED ON THE THREAD ID
 			//CHECK WHAT MESSAGE LOOKS LIKE
 			var message = req.body.message;
-
 			knex('messages')
 			.insert(
 			{
@@ -30,7 +29,6 @@ router.route('/')
 				knex('thread_participants')
 				.insert({thread_id:thread_id[0], user_id:data[0].sender_id})
 				.then(()=>{
-
 			  	knex('thread_participants')
 			  	.insert({thread_id:thread_id[0], user_id:data[0].rec_id})
 			  	.then(()=>{})
