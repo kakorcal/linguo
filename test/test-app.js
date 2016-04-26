d5165a8ac0cfb8db5020d2c941b50f0669aeec66
Andrew Furth <andrew.furth@gmail.com>
2016-04-26 16:30:39 -0700

    beforeEach and afterEach migrations and seeds set up in test-app.js

--- test/test-app.js
+++ test/test-app.js
@@ -4,3 +4,21 @@ const request = require('supertest');
 const expect = require('chai').expect;
 const app = require('../app');
 const knex = require('../db/knex');
+
+beforeEach((done) => {
+  knex.migrate.latest().then(() => {
+    knex.seed.run().then(() => {
+      knex('users').then((users) => {
+        allUsers = users;
+        done();
+      });
+    });
+  });
+});
+
+afterEach((done) => {
+  knex.migrate.rollback()
+  .then(() => {
+    done();
+  });
+});
