d5157814d62f4d8bbefdfaccb29ba0da9aae07b6
Andrew Furth <andrew.furth@gmail.com>
2016-04-27 12:19:38 -0700

    changes to testing made; testing paused (and scope limited) because of challenge of simulating Google oAuth in supertest

--- test/test-app.js
+++ test/test-app.js
@@ -25,11 +25,12 @@ afterEach((done) => {
   });
 });
 
+// GET Home
 describe('GET /', () => {
   it('gets the home page', done => {
     request(app)
     .get('/')
-    .expect('Content-Type', 'text/html')
+    .expect('Content-Type', 'text/html; charset=utf-8')
     .end((err, res) => {
       expect(res.status).to.equal(200)
       done();
@@ -37,11 +38,12 @@ describe('GET /', () => {
   })
 })
 
+// GET All Users
 describe('GET /users', () => {
   it('gets the users index page', done => {
     request(app)
     .get('/users')
-    .expect('Content-Type', 'text/html')
+    .expect('Content-Type', 'text/html; charset=utf-8')
     .end((err, res) => {
       expect(res.status).to.equal(200)
       done();
@@ -50,11 +52,53 @@ describe('GET /users', () => {
   it('retrieves all the users for index page', done => {
     request(app)
     .get('/users')
-    .expect('Content-Type', 'text/html')
+    .expect('Content-Type', 'text/html; charset=utf-8')
     .end((err, res) => {
-      expect(res.body.length).to.equal(allUsers.length)
+      // console.log('RESBODY', res)
+      // console.log('ERRORRRRRRRR', err)
+      expect(res.text).to.contain('Thomas')
+      expect(res.text).to.contain('Dumbledore')
+      expect(res.text).to.contain('Homer')
       done();
     })
   })
 })
+
+//***************************************************************************
+//TESTING IS LIMITED BECAUSE SIMULATING A USER LOGGED IN VIA
+// GOOGLE oAuth IS GOING TO TAKE MORE TIME THAN WE WANT
+// 
+// MATT SAID TO MOVE ON AND PRIORITIZE OTHER FEATURES
+// GIVEN TIME CONSTRAINTS OF THIS PROJECT
+//***************************************************************************
+
+// ***THIS TEST FAILS B/C TEST USERS CANNOT LOG IN
+// GET a Specific User
+describe('GET users/1', () => {
+  it('gets the user\'s show page', (done) => {
+    request(app)
+    .get('/users/1')
+    .expect('Content-Type', 'text/html; charset=utf-8')
+    .end((err, res) => {
+      expect(res.text).to.contain('Current User: Thomas')
+      done();
+    })
+  })
+})
+  // Is the user logged in
+  // Does the user's page load
+  // Is the user's information displayed
+  // If current user is different, are they redirected to the /users page
+
+// GET User Edit Page
+
+// INSERT a User (using Google oAuth) -- CAN THIS BE TESTED??
+
+// PUT Update a User
+
+// DEL a User
+
+// INSERT a New Thread
+
+// GET a Specific Thread 
 
